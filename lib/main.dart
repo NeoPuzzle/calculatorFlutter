b77b19c86ed7c91a5ffc5cff066ca7e56c45e64c
rbenites <alrrebe@gmail.com>
2024-08-27 23:00:06 -0500

    add main container

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main(List<String> args) {
+  runApp(const CalcApp());
+}
+
+
+class CalcApp extends StatefulWidget {
+  const CalcApp({super.key});
+
+  @override
+  State<CalcApp> createState() => _CalcAppState();
+}
+
+class _CalcAppState extends State<CalcApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Calculadora",
+      home: Scaffold(
+        backgroundColor: const Color(0xFF283637),
+        body: Container(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                alignment: const Alignment(1.0, 1.0),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Text("Historial",
+                  style: GoogleFonts.rubik
+                  (textStyle: const TextStyle(fontSize: 24, color: Color(0xFF545F61))),),
+                ),
+              )
+            ],
+          )
+        ),
+      )
+    );
+  }
+}
